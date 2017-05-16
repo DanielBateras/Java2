3f004e0d651e7dd4fe46084d88cdb09b93e42766
DanielBateras <danielbateras@hotmail.com>
2017-05-16 17:47:50 -0300

    Add files via upload

---
+++ Java2/src/br/edu/ifsp/livro/Controle.java
@@ -0,0 +1,32 @@
+package br.edu.ifsp.livro;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+
+
+public class Controle {
+
+	public static void main(String[] args) throws IOException {
+		
+		 Parametro p = new Parametro();
+		 p.lerParametro();
+		 
+		 
+		Leitura l = new Leitura();
+		 l.lerArquivo();
+		
+		
+		 
+		
+		
+		
+
+}
+}
+
---
+++ Java2/src/br/edu/ifsp/livro/LeituraPdf.java
@@ -0,0 +1,44 @@
+package br.edu.ifsp.livro;
+
+/*import java.io.*;
+import org.apache.pdfbox.pdmodel.*;
+import org.apache.pdfbox.util.*;*/
+
+
+public class LeituraPdf {
+	
+	
+	/*public static String extraiTextoDoPDF(String caminho) {
+	    PDDocument pdfDocument = null;
+	    try {
+	      pdfDocument = PDDocument.load(caminho);
+	      PDFTextStripper stripper = new PDFTextStripper();
+	      String texto = stripper.getText(pdfDocument);
+	      return texto;
+	    } catch (IOException e) {
+	      throw new RuntimeException(e);
+	    } finally {
+	      if (pdfDocument != null) try {
+	        pdfDocument.close();
+	      } catch (IOException e) {
+	        throw new RuntimeException(e);
+	      }
+	    }
+	  }
+
+	  /**
+	  *
+	  * Extrai o conteudo do arquivo indicado
+	  *
+	  
+	  public static void main(String[] args) {
+	    String caminho = "/Users/marcoreis/Downloads/cpc_L5869.pdf";
+	    String texto = extraiTextoDoPDF(caminho);
+	    System.out.println(texto);
+	    */
+	  }
+	  
+	
+	
+
+
